22a6d5f29b6b4044e69ae2d5853b3a27cea318a1
nster98 <nster98@gmail.com>
2021-05-13 17:09:40 -0400

    Made a more presentable framework for displaying games

--- game-database-angular/src/app/game.ts
+++ game-database-angular/src/app/game.ts
@@ -2,4 +2,5 @@ export interface Game {
   title: String;
   platform: String;
   completed_date: String;
+  time_taken: String;
 }
--- game-database-angular/src/app/games/games.component.ts
+++ game-database-angular/src/app/games/games.component.ts
@@ -12,17 +12,21 @@ export class GamesComponent implements OnInit {
     {
       title: 'A Short Hike',
       platform: 'PC',
-      completed_date: '2020-09-03'
+      completed_date: '2020-09-03',
+      time_taken: '1:00:00'
     },
     {
       title: 'Astral Chain',
       platform: 'Switch',
-      completed_date: '2019-09-11'
+      completed_date: '2019-09-11',
+      time_taken: '17:24:19'
+
     },
     {
       title: 'The Last of Us',
       platform: 'PlayStation 3',
-      completed_date: '2020-08-01'
+      completed_date: '2020-08-01',
+      time_taken: '20:00:00'
     }
   ];
 
